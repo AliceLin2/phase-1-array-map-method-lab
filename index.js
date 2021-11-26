e34c7a571f3dd7205901cf14a9b0d960575fe1a2
shu Lin <bbqlinshu@gmail.com>
2021-11-26 18:43:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial)=>{
+    const temps = tutorial.split(' ');
+    const toMerge = temps.map((word)=>{
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    return toMerge.join(' ')  
+  })
 }
